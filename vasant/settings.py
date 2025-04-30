e1300c347dd4080d8ae9e7db12822ba041701939
vasantachaw <vasantachaw@gmail.com>
2025-04-30 15:01:02 +0545

    update requirements.txt fiel

--- vasant/settings.py
+++ vasant/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic (optional)
-STATICFILES_DIRS = [
-    BASE_DIR / 'vasant' / 'static',
-]
+
 # Media files (uploads like images, PDFs)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
